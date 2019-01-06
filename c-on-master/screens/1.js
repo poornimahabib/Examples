d728d1e3fed57e70d81d7d92e964551ac71cdbb4
Unknown <poornima@carmelsolutions.net>
2019-01-06 21:44:38 +0530

    updated on 6/1/19

---
+++ c-on-master/screens/1.js
@@ -0,0 +1,26 @@
+function myFunction1() {
+    document.getElementById("myDropdown1").classList.toggle("show");
+} 
+function multiplyNode(node, count, deep) {
+    for (var i = 0, copy; i < count - 1; i++) {
+        copy = node.cloneNode(deep);
+        node.parentNode.insertBefore(copy, node);
+    }
+}
+multiplyNode(document.querySelector(".course_list"), 5, true)
+multiplyNode(document.querySelector(".list1"), 5, true)
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn1')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content1");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+
